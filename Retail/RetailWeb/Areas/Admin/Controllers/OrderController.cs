3b73d8b3688e6e2584ee447479a15395b5ece965
Arif Iqbal <arif_iqbal@yahoo.com>
2025-04-14 15:08:28 -0500

    Order List UI to Make Status Selected

--- Retail/RetailWeb/Areas/Admin/Controllers/OrderController.cs
+++ Retail/RetailWeb/Areas/Admin/Controllers/OrderController.cs
@@ -1,8 +1,10 @@
-﻿using System.Runtime.InteropServices.Marshalling;
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices.Marshalling;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Retail.DataAccess.Repository.IRepository;
 using Retail.Models;
+using Retail.Utility;
 
 namespace RetailWeb.Areas.Admin.Controllers
 {
@@ -25,14 +27,34 @@ namespace RetailWeb.Areas.Admin.Controllers
         #region API CALLS
 
         [HttpGet]
-        public IActionResult GetAll()
+        public IActionResult GetAll(string status)
         {
-            List<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+            IEnumerable<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+
+            //Filtering the Status after passed through GeAlla function as parameter
+            switch (status)
+            {
+                case "pending":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.PaymentStatus == SD.PaymentStatusDelayedPayment);
+                    break;
+                case "inprocess":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.StatusInProcess);
+                    break;
+                case "completed":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.StatusShipped);
+                    break;
+                case "approved":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.StatusApproved);
+                    break;
+                default:
+                    break;
+            }
+
             return Json(new { data = objOrderHeaders });
         }
 
-    }
         #endregion
+    }
 
 }
 
--- Retail/RetailWeb/wwwroot/js/order.js
+++ Retail/RetailWeb/wwwroot/js/order.js
@@ -1,14 +1,40 @@
 var dataTable;
 
 $(document).ready(function () {
-    loadDataTable();
+    //Getting URL Location
+    var url = window.location.search;
+
+    if (ur.includes("inprocess")) {
+        loadDataTable("inprocess");
+    }
+    else {
+        if (url.includes("completed")) {
+            loadDataTable("completed");
+        }
+        else {
+            if (url.includes("pending")) {
+                loadDataTable("pending");
+            }
+            else {
+                if (url.includes("approved")) {
+                    loadDataTable("approved");
+                }
+                else {
+                    loadDataTable("all");
+                }
+
+            }
+            
+        }
+
+    }
 });
 
-function loadDataTable() {
+function loadDataTable(status) {
     dataTable = $('#tblData').DataTable({
-        ajax: { url: '/Admin/order/getall' },
-        columns: [
-            { data: 'id', "width" : "25%"},
+        "ajax": { url: '/admin/order/getall?status=' + status },
+        "columns": [
+            { data: 'id', "width" : "5%"},
             { data: 'name', "width": "15%" },
             { data: 'phoneNumber', "width": "10%" },
             { data: 'applicationUser.email', "width": "15%" },
@@ -18,7 +44,7 @@ function loadDataTable() {
                 data: "id",
                 "render": function (data) {
                 return `<div class="w-75 btn-group" role="group">
-                <a href = "/admin/product/upsert?id=${data}" class = "btn btn-primary mx-2"><i class = " bi bi-pencil-square"></i>>Edit</a>
+                <a href="/admin/order/details?orderId=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i></a>
                 </div >`
                 },
                 "width" : "25%"
